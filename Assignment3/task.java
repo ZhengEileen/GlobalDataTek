689e6a440fb355a116f1d1566f7a9d306df406b6
Eileen Zheng <98563667+ZhengEileen@users.noreply.github.com>
2024-06-03 11:46:48 -0400

    Improved task.java indentation for better readability

--- Assignment3/task.java
+++ Assignment3/task.java
@@ -1,52 +1,51 @@
 package Assignment3;
 
-public class task{
-		
-		private int TaskId;
-		private String TaskTitle;
-		private String TaskText;
-		private String assignedTo;
-		
-		public task() {}
-		
-		public task(int TaskId, String TaskTitle, String TaskText, String assignedTo) {
-			this.TaskId = TaskId;
-			this.TaskTitle = TaskTitle;
-			this.TaskText = TaskText;
-			this.assignedTo = assignedTo;
-		}
-		
-		public int GetTaskId() {
-			return TaskId; 
-		}
-		
-		public void SetTaskId(int TaskId) {
-			this.TaskId = TaskId;
-		}
-		
-		public String GetTaskTitle() {
-			return TaskTitle;
-		}
-		
-		public void SetTaskTitle(String TaskTitle) {
-			this.TaskTitle = TaskTitle;
-		}
-		
-		public String GetTaskText() {
-			return TaskText;
-		}
-		
-		public void SetTaskText(String TaskText) {
-			this.TaskText = TaskText;
-		} 
-		
-		public String GetassignedTo() {
-			return assignedTo;
-		}
-		
-		public void SetassignedTo(String assignedTo) {
-			this.assignedTo = assignedTo;
-		}
-		
-	}
-
+public class task {
+    
+    private int TaskId;
+    private String TaskTitle;
+    private String TaskText;
+    private String assignedTo;
+    
+    public task() {}
+    
+    public task(int TaskId, String TaskTitle, String TaskText, String assignedTo) {
+        this.TaskId = TaskId;
+        this.TaskTitle = TaskTitle;
+        this.TaskText = TaskText;
+        this.assignedTo = assignedTo;
+    }
+    
+    public int GetTaskId() {
+        return TaskId; 
+    }
+    
+    public void SetTaskId(int TaskId) {
+        this.TaskId = TaskId;
+    }
+    
+    public String GetTaskTitle() {
+        return TaskTitle;
+    }
+    
+    public void SetTaskTitle(String TaskTitle) {
+        this.TaskTitle = TaskTitle;
+    }
+    
+    public String GetTaskText() {
+        return TaskText;
+    }
+    
+    public void SetTaskText(String TaskText) {
+        this.TaskText = TaskText;
+    } 
+    
+    public String GetassignedTo() {
+        return assignedTo;
+    }
+    
+    public void SetassignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+    
+}
