cc7863d126039c6091e0fc1cb4e318afbbb9e245
Eileen Zheng <98563667+ZhengEileen@users.noreply.github.com>
2024-06-03 15:30:09 -0400

    Update Main.java

--- Assignment3/Main.java
+++ Assignment3/Main.java
@@ -3,17 +3,16 @@ package Assignment3;
 import java.util.Scanner;
 
 public class Main {
-    
-    public static void main(String[] args) {
-    
-        Scanner UserInput = new Scanner(System.in);
-        taskservice taskService = new taskservice(100);
+	
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+        taskService taskService = new taskService(100);
         
-        taskService.Greeting();
+        taskService.greeting();
         
-        int OptionChoice = -10;
+        int optionChoice = -10;
 
-        while (OptionChoice != 0) {
+        while (optionChoice != 0) {
             System.out.println("Here are the operations you can perform: ");
             
             System.out.println("1. Add more task.");
@@ -25,28 +24,29 @@ public class Main {
             System.out.println("0. Exit");
             System.out.println("Please enter the option number to continue: ");
             
-            OptionChoice = UserInput.nextInt();
-            UserInput.nextLine(); 
+            optionChoice = userInput.nextInt();
+            userInput.nextLine(); 
 
-            if (OptionChoice == 1) {
-                taskService.AddTask();
-            } else if (OptionChoice == 2) {
-                taskService.UpdateTask();
-            } else if (OptionChoice == 3) {
-                taskService.DeleteTask();
-            } else if (OptionChoice == 4) {
-                taskService.SearchTask();
-            } else if (OptionChoice == 5) {
-                taskService.ReorderTask();
-            } else if (OptionChoice == 6) {
-                taskService.checkRepeated();
-            } else if (OptionChoice != 0) {
-                System.out.println("Invalid option number, please try again.");
+            if (optionChoice == 1) {
+            	taskService.addTask();
+            } else if (optionChoice == 2) {
+                taskService.updateTask();
+            } else if (optionChoice == 3) {
+                taskService.deleteTask();
+            } else if (optionChoice == 4) {
+            	taskService.searchTask();
+            } else if (optionChoice == 5) {
+            	taskService.reorderTask();
+            } else if (optionChoice == 6) {
+            	taskService.checkRepeated();
+            } else if (optionChoice != 0) {
+            	System.out.println("Invalid option number, please try again.");
             }
 
         }
         System.out.println("Bye Bye.");
 
-        UserInput.close();
-    }
+        userInput.close();
+	}
 }
+
