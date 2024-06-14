0f1c69f15b89f1eadf0afbd7a52e613d2812295a
Eileen Zheng <fangquan.zheng@outlook.com>
2024-06-13 20:29:16 -0400

    Add Assignment7 files

---
+++ exception/PasswordException.java
@@ -0,0 +1,7 @@
+package com.infosys.exception;
+
+public class PasswordException extends Exception {
+	public PasswordException(String message) {
+		super(message);
+	}
+}
---
+++ exception/UserLimitException.java
@@ -0,0 +1,7 @@
+package com.infosys.exception;
+
+public class UserLimitException extends Exception {
+	public UserLimitException(String message) {
+		super(message);
+	}
+}
