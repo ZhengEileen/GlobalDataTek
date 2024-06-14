9d510c47fd3c5d1b12e42f967b744550adb4888c
Eileen Zheng <98563667+ZhengEileen@users.noreply.github.com>
2024-06-13 20:58:39 -0400

    Update Exception java files

---
+++ Assignment7/exception/InvalidOptionException.java
@@ -0,0 +1,7 @@
+package com.infosys.exception;
+
+public class InvalidOptionException extends Exception {
+    public InvalidOptionException(String message) {
+        super(message);
+    }
+}
---
+++ Assignment7/exception/TaskLimitException.java
@@ -0,0 +1,7 @@
+package com.infosys.exception;
+
+public class TaskLimitException extends Exception {
+	public TaskLimitException(String message) {
+		super(message);
+	}
+}
